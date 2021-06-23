469234e1e3bdcfabcd710920bfe126d65ddc0c23
d.valenta <dziyana.valenta@bglgroup.co.uk>
2021-06-23 16:13:30 +0300

    Client is ready

---
+++ src/main/java/dvalenta/Consumer/Client/Client.java
@@ -0,0 +1,31 @@
+package dvalenta.Consumer.Client;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+@Builder
+public class Client {
+   private Long clientId;
+   private String email;
+
+   public Client(
+           @JsonProperty("clientId") Long clientId,
+           @JsonProperty("email") String email
+   ) {
+      this.clientId = clientId;
+      this.email = email;
+   }
+
+   public Long getClientId() {
+      return clientId;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+}
